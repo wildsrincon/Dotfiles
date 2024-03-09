ce4b2c55293988623812a7dd86bd463ace480c76
root <root@wichos80>
2024-03-09 11:44:09 -0500

    Adding dotfile nvim and tmux config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,14 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+-- yanky text wsl
+in_wsl = os.getenv("WSL_DISTRO_NAME") ~= nil
+
+if in_wsl then
+  vim.g.clipboard = {
+    name = "wsl clipboard",
+    copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
+    paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
+    cache_enabled = true,
+  }
+end
+
+require("config.lazy")
